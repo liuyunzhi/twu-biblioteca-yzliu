bd185a079ddffa5dd23ff4a489e11caaae1b1db8
Yunzhi Liu <liu_yun_zhi@163.com>
2018-09-09 00:51:14 +0800

    feat: completed welcome message

---
+++ src/com/twu/biblioteca/controllers/MainMenu.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca.controllers;
+
+public class MainMenu {
+
+    public static void printWelcomeMessage() {
+        System.out.println("**************************");
+        System.out.println("*                        *");
+        System.out.println("* Welcome to Biblioteca !*");
+        System.out.println("*                        *");
+        System.out.println("**************************");
+    }
+}
---
+++ test/com/twu/biblioteca/CustomerTest.java
@@ -0,0 +1,45 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.controllers.MainMenu;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+
+    private PrintStream console;
+    private ByteArrayOutputStream bytes;
+
+    @Before
+    public void setUp() {
+        bytes = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(console);
+    }
+
+    @Test
+    public void show_welcome_message() {
+        //given
+        String expected = "**************************\n"
+                + "*                        *\n"
+                + "* Welcome to Biblioteca !*\n"
+                + "*                        *\n"
+                + "**************************\n";
+
+        //when
+        MainMenu.printWelcomeMessage();
+
+        //then
+        assertEquals(expected, bytes.toString());
+    }
+}
